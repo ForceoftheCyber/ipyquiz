e46ee317ba99c0fb771e24961a6a5d72e278d82d
petvat <peter.vatno02@gmail.com>
2025-03-21 21:35:21 +0100

    refactor: rewrite question_group
    Rewrite question_group to make dynamic
    question rerendering on wrong answer easier.

--- src/ipyquizjb/questions.py
+++ src/ipyquizjb/questions.py
@@ -1,9 +1,20 @@
 import json
 import ipywidgets as widgets
-from IPython.display import display
+from IPython.display import display, clear_output
+from collections.abc import Callable
+from typing import Any
 
 from ipyquizjb.types import QuestionWidgetPackage, Question
-from ipyquizjb.question_widgets import multiple_choice, multiple_answers, no_input_question, numeric_input
+from ipyquizjb.question_widgets import (
+    multiple_choice,
+    multiple_answers,
+    no_input_question,
+    numeric_input,
+)
+
+type EvaluationFunction = Callable[[], float | None]
+type FeedbackCallback = Callable[[], None]
+type QuestionWidgetPackage = tuple[widgets.Box, EvaluationFunction, FeedbackCallback]
 
 
 def make_question(question: Question) -> QuestionWidgetPackage:
@@ -16,31 +27,49 @@ def make_question(question: Question) -> QuestionWidgetPackage:
             # TODO: Add validation of format?
             if "answers" not in question or not question["answers"]:
                 raise AttributeError(
-                    "Multiple choice should have list of possible answers (options)")
-            return multiple_choice(question=question["body"], options=question["answers"], correct_option=question["answer"][0])
+                    "Multiple choice should have list of possible answers (options)"
+                )
+            return multiple_choice(
+                question=question["body"],
+                options=question["answers"],
+                correct_option=question["answer"][0],
+            )
 
         case "MULTIPLE_CHOICE":
             # Multiple choice, multiple answer
             if isinstance(question["answer"], str):
                 raise TypeError(
-                    "question['answer'] should be a list when question type is multiple choice")
+                    "question['answer'] should be a list when question type is multiple choice"
+                )
             if "answers" not in question or not question["answers"]:
                 raise AttributeError(
-                    "Multiple choice should have list of possible answers (options)")
-            return multiple_answers(question=question["body"], options=question["answers"], correct_answers=question["answer"])
+                    "Multiple choice should have list of possible answers (options)"
+                )
+            return multiple_answers(
+                question=question["body"],
+                options=question["answers"],
+                correct_answers=question["answer"],
+            )
 
         case "NUMERIC":
             if isinstance(question["answer"], list):
                 raise TypeError(
-                    "question['answer'] should not be a list when question type is multiple choice")
-            return numeric_input(question=question["body"], correct_answer=float(question["answer"]))
+                    "question['answer'] should not be a list when question type is multiple choice"
+                )
+            return numeric_input(
+                question=question["body"], correct_answer=float(question["answer"])
+            )
 
         case "TEXT":
             solution_notes = question["notes"] if "notes" in question else []
 
             # Will always be considered a right solution (does not influence score computation)
-            always_correct = (lambda: True)
-            return no_input_question(question=question["body"], solution=solution_notes), always_correct, (lambda: None)
+            always_correct = lambda: True
+            return (
+                no_input_question(question=question["body"], solution=solution_notes),
+                always_correct,
+                (lambda: None),
+            )
 
         case _:
             raise NameError(f"{question['type']} is not a valid question type")
@@ -57,10 +86,9 @@ def singleton_group(question: Question):
     def _inner_check(button):
         callback()
 
-    button = widgets.Button(description="Check answer", icon="check",
-                            style=dict(
-                                button_color="lightgreen"
-                            ))
+    button = widgets.Button(
+        description="Check answer", icon="check", style=dict(button_color="lightgreen")
+    )
     button.on_click(_inner_check)
 
     return widgets.VBox([widget, button])
@@ -79,13 +107,15 @@ def display_questions(questions: list[Question], as_group=True):
             # We are currently only interesting in displaying the question widget
             # and do not care about the eval
             display(singleton_group(question))
+
     # TODO
 
 
-def question_group(questions: list[Question]) -> widgets.Box:
+def question_group(questions: list[Question]) -> widgets.Output:
     """
+    TODO: Update docs.
     Makes a VBox of all the questions.
-    Has a separate field for output feedback for the whole group, 
+    Has a separate field for output feedback for the whole group,
     evaluated based on a cumulation of the evaluation functions of each question.
 
     VBox
@@ -93,48 +123,67 @@ def question_group(questions: list[Question]) -> widgets.Box:
     Output
     """
 
-    question_boxes, eval_functions, feedback_callbacks = zip(
-        *(make_question(question) for question in questions))
+    # A group of questions is referred to as a 'quiz'. Could rename back to group if confusing.
 
-    def group_evaluation():
-        group_sum = 0
-        for func in eval_functions:
-            group_sum += func() if func() else 0
-        return group_sum
+    output = widgets.Output()  # This the output for *this* group (or quiz).
 
-    def feedback(evaluation: float):
-        max_score = len(questions)
-        if evaluation == max_score:
-            return "All questions are correct!!"
-
-        return "Wrong!!"
-
-    output = widgets.Output()
-
-    def _inner_check(button):
+    def render_quiz():
         with output:
-            output.outputs = [
-                {'name': 'stdout', 'text': feedback(group_evaluation()), 'output_type': 'stream'}]
+            clear_output(wait=True)
+            # TODO: Shuffle list.
+            display(build_quiz()) # NOTE: As of now, this simply displays the questions in the same way as before. 
+                                  # NOTE: Maybe we could have a num_questions parameter in question_group,
+                                  # NOTE: and then shuffle the questions before rerendering a subset of the questions.
 
-        for callback in feedback_callbacks:
-            callback()
+    def build_quiz() -> widgets.Box:
+        # Unpack
+        question_boxes, eval_functions, feedback_callbacks = zip(
+            *(make_question(question) for question in questions)
+        )
 
-    button = widgets.Button(description="Check answer", icon="check",
-                            style=dict(
-                                button_color="lightgreen"
-                            ))
-    button.on_click(_inner_check)
+        def _inner_check(button):
+            # Rename this function to e.g. 'check_answers'?
 
-    questions_box = widgets.VBox(question_boxes, layout=dict(
-        border="solid"
-    ))
+            def group_evaluation() -> float:
+                return sum(func() if func() else 0 for func in eval_functions)
 
-    return widgets.VBox([questions_box, button, output])
+            def approved(evaluation: float):
+                return len(questions) == evaluation
+
+            def feedback(approved: bool):
+                # Maybe have evaluation here as argument to have more fine-grained feedback messages.
+                return "All questions are correct!" if approved else "Wrong!"
+
+            is_approved = approved(group_evaluation())
+
+            with feedback_output:
+                print(feedback(is_approved))
+
+            for callback in feedback_callbacks:
+                callback()
+
+            
+        # The text output.
+        feedback_output = widgets.Output()
+
+        check_btn = widgets.Button(
+            description="Check answer",
+            icon="check",
+            style=dict(button_color="lightgreen"),
+        )
+        check_btn.on_click(_inner_check)
+
+        questions_box = widgets.VBox(question_boxes, layout=dict(border="solid"))
+
+        return widgets.VBox([questions_box, check_btn, feedback_output])
+
+    render_quiz()
+    return output
 
 
 def display_json(questions: str, as_group=True):
     """
-    Helper function for displaying based on the json-string from the FaceIT-format. 
+    Helper function for displaying based on the json-string from the FaceIT-format.
     """
 
     questions_dict = json.loads(questions)
